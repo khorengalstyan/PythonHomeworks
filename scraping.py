6ea72bd4b1230cc134e3fd5ed8a372b17de2b62d
Khoren <90375268+khorengalstyan@users.noreply.github.com>
2023-10-21 16:03:31 +0400

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,24 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 from collections import defaultdict
+import urllib3
 
+
+urllib3.disable_warnings()
 
 
 def quotaCount(pages):
     url = 'https://quotes.toscrape.com/page/'
     count = defaultdict(int)
-    for page in range(1, pages):
+    for page in range(1, int(pages)):
         urlPage = url + str(page)
-        resp = requests.get(urlPage)
+        response = requests.get(urlPage, verify=False)
 
-        if (resp.status_code == 200):
-            soup = BeautifulSoup(resp.text, 'html.parser')
+        if (response.status_code == 200):
+            soup = BeautifulSoup(response.text, 'html.parser')
             tags = soup.find_all(class_='tag')
             for tag in tags:
                 name = tag.getText()
                 count[name] += 1
         else:
-            print("Error: Something went wrong, Please try again.")
+            print("Error: Something went wrong")
     countSorted = dict(sorted(count.items(), key=lambda item: item[1], reverse=True))
     return countSorted
 
@@ -26,6 +29,13 @@ def toJson(countSorted):
     for toJson in countSorted:
         print("{\n", "     ", "\"", toJson, "\":" , " ", "\"",countSorted.get(toJson),"\"\n}")
 
-pages = 10
+
+pages = input("Please enter how many pages you want to check:")
+
+try:
+    int(pages)
+except:
+    raise TypeError("Please input valid page")
+
 countSorted = quotaCount(pages)
 toJson(countSorted)
